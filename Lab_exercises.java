72dea1f163583d6e96a646c3beb994d1d96a6a23
birundha-kprcas <birundha2426@gmail.com>
2026-08-13 10:54:16 +0530

    lab C

--- Lab_exercises.java
+++ Lab_exercises.java
@@ -1,16 +1,8 @@
 public class Lab_exercises {
     public static void main(String[] args) {
-        int a = 5;
-        long b = 654443246789021565l;
-        double c = 24.267532523456;
-        boolean d = true;
-        char e = 'm';
-        System.out.println("int : "+a);
-        System.out.println("long : "+b);
-        System.out.println("double : "+c);
-        System.out.println("boolean : "+d);
-        System.out.println("char : "+e);
-
-
+       // explicit casting
+        double value=9.7;
+        System.out.println((int) value);
+        System.out.println((int) Math.round(value));
     }
 }
