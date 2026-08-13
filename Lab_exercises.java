1defa919de12da9454920059a93a75dd2086af5c
birundha-kprcas <birundha2426@gmail.com>
2026-08-13 10:41:40 +0530

    lab A

---
+++ Lab_exercises.java
@@ -0,0 +1,16 @@
+public class Lab_exercises {
+    public static void main(String[] args) {
+        int a = 5;
+        long b = 654443246789021565l;
+        double c = 24.267532523456;
+        boolean d = true;
+        char e = 'm';
+        System.out.println("int : "+a);
+        System.out.println("long : "+b);
+        System.out.println("double : "+c);
+        System.out.println("boolean : "+d);
+        System.out.println("char : "+e);
+
+
+    }
+}
