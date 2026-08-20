af28007e1c634cc984b6c33562a849624938faf3
birundha-kprcas <birundha2426@gmail.com>
2026-08-20 19:31:33 +0530

    2)lab A

--- Lab_exercises.java
+++ Lab_exercises.java
@@ -1,11 +1,11 @@
 public class Lab_exercises {
     public static void main(String[] args) {
-     //division trap
-        int a = 5/2;
-        double b = 5/2;
-        double c = 5.0/2;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+     //arithmetic
+        int a=17;
+        double b=17.0;
+        int c=5;
+        System.out.println(a/c);//return int
+        System.out.println(b/c);//return double
+        System.out.println(a%c);//return int
     }
 }
