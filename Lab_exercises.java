b6c1bd8fe2aacb0123a00fa0fe8dee39963ebfb8
birundha-kprcas <birundha2426@gmail.com>
2026-08-20 19:59:04 +0530

    2)lab E

--- Lab_exercises.java
+++ Lab_exercises.java
@@ -1,12 +1,12 @@
 public class Lab_exercises {
     public static void main(String[] args) {
-        int choice = 2;
-        String message = switch (choice) {
-            case 1 -> "Add";
-            case 2 -> "View";
-            case 3 -> "Exit";
-            default -> "Invalid";
-        };
-        System.out.println(message);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }int i = 5;
+
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
     }
 }
